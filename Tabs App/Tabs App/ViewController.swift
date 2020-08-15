beca3bf2678422aaf6c8194f0d69cb0f69ac594d
Araad Shams <araadshams2003@gmail.com>
2020-08-15 17:44:03 -0400

    NewCommit

---
+++ Tabs App/Tabs App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabs App
+//
+//  Created by Wania Shams on 15/08/2020.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tabs App/Tabs AppTests/Tabs_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tabs_AppTests.swift
+//  Tabs AppTests
+//
+//  Created by Wania Shams on 15/08/2020.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import XCTest
+@testable import Tabs_App
+
+class Tabs_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
