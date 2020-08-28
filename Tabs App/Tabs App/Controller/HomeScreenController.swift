2aae281b8d3db60718c29e33d7717c8ed4199504
Madhumita Mocharla <madhu.mocharla@gmail.com>
2020-08-28 10:16:30 -0400

    Added Home Screen View

---
+++ Tabs App/Tabs App/Controller/HomeScreenController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeScreenController.swift
+//  Tabs App
+//
+//  Created by Madhumita Mocharla on 2020-08-28.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseCore
+
+class HomeScreenController: UIViewController {
+
+    //MARK: IBOutlets
+    
+    
+    
+    
+    
+    
+}
