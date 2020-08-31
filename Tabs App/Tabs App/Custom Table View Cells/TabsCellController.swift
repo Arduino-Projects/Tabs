2ec09fe503e55dd4ba86958c808e448ecda8ab00
Araad Shams <araadshams2003@gmail.com>
2020-08-31 16:54:47 -0400

    added custom tabs cell

---
+++ Tabs App/Tabs App/Custom Table View Cells/TabsCellController.swift
@@ -0,0 +1,15 @@
+//
+//  TabsCellController.swift
+//  Tabs App
+//
+//  Created by Wania Shams on 31/08/2020.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import UIKit
+
+class TabsCell : UITableViewCell {
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblWhoOwes: UILabel!
+    @IBOutlet weak var lblAmount: UILabel!
+}
