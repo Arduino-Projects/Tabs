f74f9290332aa56e656f29ed7d19aaf167b61c73
Araad Shams <araadshams2003@gmail.com>
2020-08-31 16:58:07 -0400

    missed a spot

---
+++ Tabs App/Tabs App/Controller/TabsController.swift
@@ -0,0 +1,44 @@
+//
+//  TabsController.swift
+//  Tabs App
+//
+//  Created by Wania Shams on 31/08/2020.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class TabsController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var tbvTabs: UITableView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        tbvTabs.register(UINib(nibName: "TabsCell", bundle: nil), forCellReuseIdentifier: "TabsCell")
+        tbvTabs.delegate = self
+        tbvTabs.dataSource = self
+        print("I AM HERE!")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TabsCell", for: indexPath) as! TabsCell
+        
+        cell.lblName.text! = "Abed Nadir"
+        cell.lblWhoOwes.text! = "you owe"
+        cell.lblAmount.text! = "$69420"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
