3ce9c89c9c974e176e3323b48447faeff8f75e54
Araad Shams <araadshams2003@gmail.com>
2020-09-08 10:36:02 -0400

    added iboutlets

---
+++ Tabs App/Tabs App/Controller/IndividualBetController.swift
@@ -0,0 +1,40 @@
+//
+//  IndividualBetController.swift
+//  Tabs App
+//
+//  Created by Araad Shams on 2020-09-08.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import UIKit
+
+class IndividualBetController : UIViewController {
+    
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblFriendName: UILabel!
+    @IBOutlet weak var lblBetDescription: UILabel!
+    @IBOutlet weak var lblAmount: UILabel!
+    @IBOutlet weak var sgvBetState: UISegmentedControl!
+    @IBOutlet weak var stsLoadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var lblInstructionIndicator: UILabel!
+    @IBOutlet weak var btnCloseBetIndicator: UIButton!
+    @IBOutlet weak var btnRefreshBet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblBetDescription.numberOfLines = 0;
+        lblBetDescription.sizeToFit()
+        
+    }
+    
+    
+    @IBAction func closeBetPressed(_ sender: Any) {
+    }
+    
+    @IBAction func refreshButtonPressed(_ sender: Any) {
+    }
+    @IBAction func betStateChanged(_ sender: Any) {
+    }
+    
+}
---
+++ Tabs App/Tabs App/Controller/OverallTabController.swift
@@ -0,0 +1,13 @@
+//
+//  OverallTabController.swift
+//  Tabs App
+//
+//  Created by Araad Shams on 2020-09-08.
+//  Copyright © 2020 Araad Shams. All rights reserved.
+//
+
+import UIKit
+
+class OverallTabController : UIViewController {
+    
+}
